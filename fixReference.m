c50221a9ba67673a84f0af14028e79ad8e1c23d5
Santi Acosta <acosta.santi.4@gmail.com>
2021-10-27 18:48:00 -0700

    Minor errors in fixReference
    The rotation was not set right. I do not understand why, it used to work quite well. New version issues?

--- fixReference.m
+++ fixReference.m
@@ -1,11 +1,22 @@
-function fixed_reference = fixReference(RAW_REFERENCE_FILENAME)
+function corrected_reference = fixReference(RAW_FILENAME, varargin)
 % fixReference(RAW_REFERENCE_FILENAME) rotate any image and translates it
 % so that it is centered and straight. 
 %
-% First 
+% First, the function will prompt the user to draw a line along the saggital
+% line to make the reference straight. After double clicking it, the user
+% will have to draw a circle to surround the cranial window. After
+% positioning it, the user will have to press enter to finish, space to
+% repeat or esc to exit. Then, the corrected reference will be saved in the
+% current directory.
 % 
 % INPUTS:
-%       UNFIXED_REFERENCE_FILENAME: string, filename
+%       UNFIXED_REFERENCE_FILENAME: string, filename (must be in tif
+%       format)
+%
+%       varargin:
+%                'save': if the user wants to automatically save the
+%                corrected reference. If so, the original file must contain
+%                the word 'raw' in it.
 %
 % OTPUT:
 %       fixed_reference: N x N matrix with fixed imaged, where N is num
@@ -15,13 +26,19 @@ function fixed_reference = fixReference(RAW_REFERENCE_FILENAME)
 % Developed by SAM on 11/15/2020. Based on code developed by LFM.
 % Last updated 10/27/2021.
 
-%% Importing and labeling input image
+%% Importing input image and enforcing tif format
 
-[~, file_name, ext] = fileparts(RAW_REFERENCE_FILENAME);
-unfixed_reference = imread(file_name);
+flag_save = ismember('save', varargin);
+[~, file_name, ext] = fileparts(RAW_FILENAME);
+
+if ~strcmp(ext, {'.tif', '.tiff'})
+    error('Input filename must be in tif format')                           
+end
+
+raw_reference = imread(RAW_FILENAME);
 
 %% Figure options 
-fig_opt = {'Color', [0 0 0], 'Position', [200 200 725 725]};
+fig_opt = {'Color', [0.05 0.05 0.05], 'Position', [200 200 725 725]};
 
 ax_opt = {'xticklabel',[],'yticklabel',[], 'XColor',[0 0 0], 'YColor',[0 0 0],...
     'Colormap',colormap('gray'), 'DataAspectRatio', [1 1 1]};
@@ -33,11 +50,13 @@ t_opt = {'FontName', 'CMU Bright', ...
 isSuccess = false;
 while ~isSuccess
     
-    figure(fig_opt{:}, 'Position', [200 200 1200 600]),
+    fig = figure(fig_opt{:}, 'Position', [200 200 1200 600]);
+    set(fig, 'Name', 'Draw line along sagittal fisure. Double click when done');
+    
     subplot(1, 2, 1)
-    imagesc(unfixed_reference),
+    imagesc(raw_reference),
     set(gca, ax_opt{:}),
-    title('Original Reference', t_opt{:});
+    title('Raw Reference', t_opt{:});
     
     try
         h = drawline('Position',h.Position,'Color','w');
@@ -46,12 +65,13 @@ while ~isSuccess
     end
     
     customWait(h);
+    set(fig, 'Name', 'Draw circle surrounding cranial window');
     
     % Rotation
     line_param = polyfit(h.Position(:,1), h.Position(:,2), 1);
     angle = 90 - atand(line_param(1));
-    fixed_reference = imrotate(unfixed_reference, -angle + 180, 'crop');
-     
+    corrected_reference = imrotate(raw_reference, -angle, 'crop');
+  
     % Centering
     try
         c = drawcircle('Center',c.Center,'Radius',c.Radius, ...
@@ -62,27 +82,52 @@ while ~isSuccess
     
     customWait(c);
     
-    translation = round(size(fixed_reference) / 2) - c.Center;
-    fixed_reference = imtranslate(fixed_reference, translation);
+    translation = round(size(corrected_reference) / 2) - c.Center;
+    corrected_reference = imtranslate(corrected_reference, translation);
+    
+    set(fig, 'Name', 'Enter if done. Space to repeat. Esc to exit');
     
     subplot(1, 2, 2)
-    imagesc(fixed_reference),
+    imagesc(corrected_reference),
     set(gca, ax_opt{:}),
-    title('Processed', t_opt{:});
+    title('Corrected', t_opt{:});
      
     pause(1.5)
     
-    repetition_flag = questdlg('Is everything as expected?',...
-        'Checkpoint','Yes', 'No', 'Yes');
+    waitforbuttonpress;
+    button = double(get(gcf,'CurrentCharacter'));
     
-    if strcmp(repetition_flag,'Yes')
-        isSuccess = true;
+    switch button
+        
+        case 27                     % ESC: Exit altogether
+            pause(1.2)
+            close(fig)
+            return
+            
+        case 13
+            isSuccess = true;       % INTRO: Done
+            pause(1.2);
+            close(fig)
+            
+        case 32                     % SPACE: Repeat
+            pause(1.2);
+            close(fig)
+            continue
+            
+        otherwise
+            msg = msgbox('Key not recognized. Please use escp/ent/space next time');
+            pause(1.2);
+            close(msg);
+            continue
+            
     end
     
 end
-close(gcf)
 
-imwrite(fixed_reference, strcat(mouse,'_ref_fixed.tif'),'tif');
+if flag_save
+    corrected_filename = strrep(file_name, 'raw', 'corrected');
+    imwrite(corrected_reference, strcat(corrected_filename, '.tif'), 'tif');
+end
 
     function pos = customWait(hROI)
         
